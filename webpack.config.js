5d837db26f783184a6026d043be44d9a5af25676
João Costa <costav1402@gmail.com>
2022-07-05 21:06:01 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,50 @@
-const path = require("path");
+const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-module.exports={
-    mode:'development',
-    entry:'./src/index.tsx',
-    output: {
-        path: path.join(__dirname, 'public/js'),
-        publicPath:'/public/js',
-        filename:'bundle.js'
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        alias: {
-            '@': path.join(__dirname, 'src')
+    {
+      test: /\.scss$/,
+      use: [{
+        loader: 'styled-loader'
+      }, {
+        loader: 'css-loader',
+        option: {
+          modules: true
         }
-    },
-    devServer: {
-        contentBase: './public',
-        writeToDisk:true,
-        historyApiFallback: true
-    }, 
-    externals: {
-        react: 'React',
-       'react-dom':'ReactDOM'
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ]
+      }, {
+        loader: 'sass-loader'
+      }],
+      exclude: /node_modules/
+    }]
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
